44fef87e28050527ddc86fbd7e1284314ee39e01
DevFelipeNunes <devfelipenunes@gmail.com>
2023-11-26 14:59:44 -0300

    feat: book database

---
+++ scripts/deploy.ts
@@ -0,0 +1,18 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const bookDatabase = await ethers.deployContract("BookDatabase");
+
+  await bookDatabase.waitForDeployment();
+
+  const address = await bookDatabase.getAddress();
+
+  console.log(`Deploy finished! ${address}`);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
---
+++ test/BookDatabase.test.ts
@@ -0,0 +1,73 @@
+import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { expect } from "chai";
+import { ethers } from "hardhat";
+
+describe("BookDatabase", function () {
+  // We define a fixture to reuse the same setup in every test.
+  // We use loadFixture to run this setup once, snapshot that state,
+  // and reset Hardhat Network to that snapshot in every test.
+  async function deployFixture() {
+    // Contracts are deployed using the first signer/account by default
+    const [owner, otherAccount] = await ethers.getSigners();
+
+    const BookDatabase = await ethers.getContractFactory("BookDatabase");
+    const bookDatabase = await BookDatabase.deploy();
+
+    return { bookDatabase, owner, otherAccount };
+  }
+
+  it("Should Hello World", async function () {
+    const { bookDatabase, owner, otherAccount } = await loadFixture(
+      deployFixture
+    );
+    const count = await bookDatabase.count();
+    expect(count).to.equal(0);
+  });
+
+  it("Should add book", async function () {
+    const { bookDatabase, owner, otherAccount } = await loadFixture(
+      deployFixture
+    );
+
+    await bookDatabase.addBook({ title: "New book", year: 2023 });
+
+    const count = await bookDatabase.count();
+    expect(count).to.equal(1);
+  });
+
+  it("Should edit book", async function () {
+    const { bookDatabase, owner, otherAccount } = await loadFixture(
+      deployFixture
+    );
+
+    await bookDatabase.addBook({ title: "New book", year: 2023 });
+    await bookDatabase.editBook(1, { title: "New Book 2.0", year: 2023 });
+
+    const book = await bookDatabase.books(1);
+    expect(book.title).to.equal("New Book 2.0");
+  });
+
+  it("Should remove book", async function () {
+    const { bookDatabase, owner, otherAccount } = await loadFixture(
+      deployFixture
+    );
+
+    await bookDatabase.addBook({ title: "New book", year: 2023 });
+    await bookDatabase.removeBook(1);
+
+    const count = await bookDatabase.count();
+
+    expect(count).to.equal(0);
+  });
+
+  it("Should NOT remove book", async function () {
+    const { bookDatabase, owner, otherAccount } = await loadFixture(
+      deployFixture
+    );
+
+    const instance = bookDatabase.connect(otherAccount);
+    await expect(instance.removeBook(1)).to.be.revertedWith(
+      "You don't have permission."
+    );
+  });
+});
